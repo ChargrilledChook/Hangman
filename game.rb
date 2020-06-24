d3af7f609578c470bf6f4b29afe02985a84a3b84
chook <dylanw212@gmail.com>
2020-06-24 17:10:52 +1000

    extract strings from game and put in module

--- game.rb
+++ game.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require_relative 'text_content'
+
 # general file until i make class decisions
 class Game
+  include TextContent
+
   attr_reader :dict
   attr_accessor :guesses, :progress, :wrong_guesses, :player, :secret_word
 
@@ -68,57 +72,5 @@ class Game
     puts game_intro
     puts secret_word
     puts round
-  end
-
-  def welcome_string
-    %(
-    Welcome to Hangman!)
-  end
-
-  def menu_text
-    %(
-    Enter your selection:
-
-    1. New game
-    2. Load game
-    3. Rules)
-  end
-
-  def rules_text
-    %(
-    Guess the hidden word one letter at a time. But be careful - 6 incorrect
-    guesses and you lose. Your correct guesses will be shown on the left and
-    your incorrect guesses on the right.
-
-    You can also attempt to guess the whole word at any time.
-
-    Enter 1 while playing to restart.
-    Enter 2 while playing to save and quit.
-
-    Good luck!
-  )
-  end
-
-  def game_intro
-    %(
-    The secret word is #{secret_word.length} letters long.
-    )
-  end
-
-  def player_prompt
-    %(
-    Enter your guess: )
-  end
-
-  def win_message
-    %(
-    You guessed correctly! The word was #{secret_word}. Well done!
-  )
-  end
-
-  def loss_message
-    %(
-    You didn't guess in time! The word was #{secret_word}. Bad luck!
-    )
   end
 end
---
+++ text_content.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# Contains majority of the strings required for the game / user feedback
+module TextContent
+  def welcome_string
+    %(
+    Welcome to Hangman!)
+  end
+
+  def menu_text
+    %(
+    Enter your selection:
+
+    1. New game
+    2. Load game
+    3. Rules)
+  end
+
+  def rules_text
+    %(
+    Guess the hidden word one letter at a time. But be careful - 6 incorrect
+    guesses and you lose. Your correct guesses will be shown on the left and
+    your incorrect guesses on the right.
+
+    You can also attempt to guess the whole word at any time.
+
+    Enter 1 while playing to restart.
+    Enter 2 while playing to save and quit.
+
+    Good luck!
+  )
+  end
+
+  def game_intro
+    %(
+    The secret word is #{secret_word.length} letters long.
+    )
+  end
+
+  def player_prompt
+    %(
+    Enter your guess: )
+  end
+
+  def win_message
+    %(
+    You guessed correctly! The word was #{secret_word}. Well done!
+  )
+  end
+
+  def loss_message
+    %(
+    You didn't guess in time! The word was #{secret_word}. Bad luck!
+    )
+  end
+end
