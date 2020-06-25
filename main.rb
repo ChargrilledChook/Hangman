058e2646446bb02c73c238c485b1b308a91be343
chook <dylanw212@gmail.com>
2020-06-25 17:11:51 +1000

    add ability to load save game file

--- main.rb
+++ main.rb
@@ -4,6 +4,28 @@ require_relative 'dic_loader'
 require_relative 'game'
 require_relative 'player'
 
-game = Game.new
-game.intro
+puts 'Enter your selection 1 new game 2 load'
 
+def get_input
+  input = gets.chomp.downcase
+  input
+end
+
+def load_save
+  game = YAML.load(File.read('save.txt'))
+  game
+end
+
+def game_start(choice)
+  if choice == '1'
+    game = Game.new
+    game.intro
+  elsif choice == '2'
+    game = load_save
+    game.intro
+  end
+end
+
+choice = get_input
+
+game_start(choice)
