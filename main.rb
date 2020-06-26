b90eaa8fcf0c2e899d5d2e72df16d50401c889ba
chook <dylanw212@gmail.com>
2020-06-26 11:48:13 +1000

    update main to reflect class changes

--- main.rb
+++ main.rb
@@ -3,29 +3,7 @@
 require_relative 'dic_loader'
 require_relative 'game'
 require_relative 'player'
+require_relative 'game_manager'
 
-print 'Enter your selection: 1 => New Game || 2 => Load Saved Game  '
-
-def get_input
-  input = gets.chomp.downcase
-  input
-end
-
-def load_save
-  game = YAML.load(File.read('save.txt'))
-  game
-end
-
-def game_start(choice)
-  if choice == '1'
-    game = Game.new
-    game.intro
-  elsif choice == '2'
-    game = load_save
-    game.intro
-  end
-end
-
-choice = get_input
-
-game_start(choice)
+console = GameManager.new
+console.intro
