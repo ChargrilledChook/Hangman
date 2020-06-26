5a5a1f8b7cab973a16be967d609d66af9b744b44
chook <dylanw212@gmail.com>
2020-06-26 12:22:33 +1000

    extract serialization to module

---
+++ save_manager.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Saves and loads games / serializes objects
+module SaveManager
+  def default_save
+    'save.txt'
+  end
+
+  def save_game(game)
+    yaml = YAML.dump(game)
+    file = File.new(default_save, 'w+')
+    file.puts(yaml)
+    file.close
+    puts save_message
+    exit
+  end
+
+  def load_save
+    game = YAML.load(File.read(default_save))
+    game
+  end
+end
